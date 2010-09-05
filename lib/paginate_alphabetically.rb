19d691894326280d87b71e07a0ec69f72dee4ce0
Alan McCann <alan@imccann.com>
2010-09-05 09:48:43 -0700

    Fix and tests to fix mixed-case alpha sorting and grouping

--- lib/paginate_alphabetically.rb
+++ lib/paginate_alphabetically.rb
@@ -6,7 +6,7 @@ module PaginateAlphabetically
 
   module ClassMethods
     def pagination_letters
-      all.sort_by{|obj| obj.send(@attribute)}.group_by {|group| group.send(@attribute)[0].chr.upcase}.keys
+      all.sort_by{|obj| obj.send(@attribute).upcase}.group_by {|group| group.send(@attribute)[0].chr.upcase}.keys
     end
 
     def first_letter
@@ -16,7 +16,7 @@ module PaginateAlphabetically
     end
 
     def alphabetical_group(letter = nil)
-      find(:all, :conditions => ["#{@attribute.to_s} LIKE ?", "#{letter || first_letter}%"], :order => @attribute)
+      find(:all, :conditions => ["#{@attribute.to_s.upcase} LIKE ?", "#{letter || first_letter}%"], :order => @attribute)
     end
   end
 end
